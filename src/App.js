5b727fe485fdc22927ec7a49b5288c9c496faa03
TR0205 <index0205@gmail.com>
2022-07-28 20:37:06 +0900

    useEffectのパターンを追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = props => {
     const [state, setState] = useState(props);
     const { name, price } = state;
+
+    useEffect(() => {
+        console.log('name変更');
+    }, [name]);
+
+    useEffect(() => {
+        console.log('componentマウント完了後1度しか呼ばれない');
+    }, []);
+
+    useEffect(() => {
+        console.log('componentマウント完了後何度も呼ばれる');
+    });
 
     return (
         <>
