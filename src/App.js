fe82b671c0ec3f256f619e30c4778a5be5d6fe5c
TR0205 <index0205@gmail.com>
2022-07-28 14:02:22 +0900

    カウンター追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+    const defaultValue = 10;
+    const [count, setCount] = useState(defaultValue);
+    const increment = () => setCount(prevState => prevState + 1);
+    const decrement = () => setCount(prevState => prevState - 1);
+    const double = () => setCount(prevState => prevState * 2);
+    const reset = () => setCount(defaultValue);
+    const devide3 = () => setCount((prevState) => prevState % 3 === 0 ? prevState / 3 : prevState);
     return (
-        <div>
-            This is a template for a react-app.
-        </div>
+        <>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={double}>x2</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={devide3}>３の倍数のときだけ３で割る</button>
+            <br />
+            count: {count}
+        </>
     );
 }
 
